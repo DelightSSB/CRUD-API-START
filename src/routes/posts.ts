3c12766d1e0154bb2436420865bc95de40e9583e
DelightSSB <crazybudderpie@gmail.com>
2026-01-05 21:40:37 -0500

    Server is now working, all files ok, changed route logic to allow filtering of completion status via query bbased routed

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -3,19 +3,26 @@ import tasks from "../data/tasks.js";
 const router = express.Router();
 
 //GET: Read all posts with optional limit query parameter
-router.get('/', (req, res, next) => {
-    const limit = Number(req.query.limit) || tasks.length;
+router.get("/", (req, res) => {
+//   const completed =
+    // req.query.completed === undefined
+    //   ? undefined
+    //   : req.query.completed === "true";
+    const completed =
+        typeof req.query.completed === "string"
+    ? req.query.completed.toLowerCase() === "true"
+    : undefined;
+  const limit = Number(req.query.limit) || tasks.length;
 
-    if(!isNaN(limit) && limit > 0){
-        res.status(200).json(tasks.slice(0, limit));
-    }
-    // TODO: Make Error middleware!
-    //  else {
-    //     res.status(400).json({error: "Invalid limit parameter"});
-    // }
+  let results = tasks;
 
-    res.status(200).json(tasks);
-})
+  if (completed !== undefined) {
+    results = results.filter(p => p.completed === completed);
+  }
+
+  res.status(200).json(results.slice(0, limit));
+});
+
 
 //GET: Read a specific post by ID
 router.get('/:id', (req, res, next) => {
@@ -31,14 +38,16 @@ router.get('/:id', (req, res, next) => {
 
 //GET: Read a specific task by completion status
 router.get('/:completed', (req, res, next) => {
-    const status = Boolean(req.params.completed);
+    const status = req.params.completed === "true";
     const filteredTasks = tasks.filter(t => t.completed === status);
     const limit = Number(req.query.limit) || tasks.length;
 
     if(!isNaN(limit) && limit > 0){
-        res.status(200).json(filteredTasks.slice(0, limit));
+        return res.status(200).json(filteredTasks.slice(0, limit));
     }
 
     res.status(200).json(filteredTasks);
 
 });
+
+export default router;
